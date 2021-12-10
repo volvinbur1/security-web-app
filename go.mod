913abf713488d0e124fc90c82ae5e7951a58c3d6
Oleh Lysenko <baryn72@gmail.com>
2021-12-10 02:25:42 +0200

    Small part of web interface

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/volvinbur1/security-web-app
 
 go 1.17
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
---
+++ web/pages/pages.go
@@ -0,0 +1,67 @@
+package pages
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+type RegisterStruct struct {
+	Email    string `json:"email" bson:"email"`
+	Password string `json:"password" bson:"password"`
+}
+
+func LoginPage(rw http.ResponseWriter, req *http.Request) {
+	path := filepath.Join("web/app", "html", "loginPage.html")
+
+	tmpl, err := template.ParseFiles(path)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+
+	err = tmpl.Execute(rw, nil)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+
+}
+func RegisterPage(rw http.ResponseWriter, req *http.Request) {
+
+	path := filepath.Join("web/app", "html", "registerPage.html")
+
+	tmpl, err := template.ParseFiles(path)
+
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+	err = tmpl.Execute(rw, nil)
+	if err != nil {
+		http.Error(rw, err.Error(), 400)
+		return
+	}
+	if req.Method == "POST" {
+		var registerStruct RegisterStruct
+
+		err = req.ParseForm()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		registerStruct.Email = req.FormValue("email")
+		password := req.FormValue("psw")
+		/*
+			some password hashing
+
+		*/
+		registerStruct.Password = password
+		fmt.Println(registerStruct)
+	} else {
+		//some error
+	}
+
+}

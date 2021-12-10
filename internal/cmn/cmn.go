cc195fb2851be8ee338f0c5d751d7af4c15cea53
Volodymyr Burdeinyi <burdeinyi.v.o@gmail.com>
2021-12-10 18:11:08 +0200

    + added phone field to user struct

--- internal/cmn/cmn.go
+++ internal/cmn/cmn.go
@@ -1,9 +1,11 @@
 package cmn
 
 type User struct {
-	Login   string `bson:"login"`
+	Login string `bson:"login"`
+
 	Name    string `bson:"name"`
 	Surname string `bson:"surname"`
+	Phone   string `bson:"phone"`
 
 	Password string `bson:"pwdhash"`
 	PwdSalt  string `bson:"pwdsalt"`
